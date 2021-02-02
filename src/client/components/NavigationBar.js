8481712774afe7e4c9d333c586e1f5d5373fbd2e
Christine Chye <ce.chye@gmail.com>
2021-02-02 15:48:36 -0800

    update

--- src/client/components/NavigationBar.js
+++ src/client/components/NavigationBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import {Link} from "react-router-dom";
 import './../styles.css';
 
 const NavigationBar = () => {
@@ -8,9 +9,12 @@ const NavigationBar = () => {
     <Navbar variant="dark" style={{backgroundColor: '#97294E'}}>
         <Navbar.Brand href="/">COVID-19 Infodemic</Navbar.Brand>
         <Nav className="mr-auto">
-            <Nav.Item><Nav.Link href='/'>Home</Nav.Link></Nav.Item>
+            <Nav.Item><Link to='/'>Home</Link></Nav.Item>
+            <Nav.Item><Link to='/findstate'>Find State</Link></Nav.Item>
+            <Nav.Item><Link to='/aboutus'>About Us</Link></Nav.Item>
+            {/* <Nav.Item><Nav.Link href='/'>Home</Nav.Link></Nav.Item>
             <Nav.Item><Nav.Link href='/findstate'>Find State</Nav.Link></Nav.Item>
-            <Nav.Item><Nav.Link href='/aboutus'>About Us</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link href='/aboutus'>About Us</Nav.Link></Nav.Item> */}
         </Nav>
     </Navbar>
   );
