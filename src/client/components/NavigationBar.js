94c6fee2e823ca0ca6924391a5890b1492dc0427
Christine Chye <ce.chye@gmail.com>
2021-02-02 15:53:41 -0800

    fixed framework, navigation, and header

--- src/client/components/NavigationBar.js
+++ src/client/components/NavigationBar.js
@@ -9,9 +9,9 @@ const NavigationBar = () => {
     <Navbar variant="dark" style={{backgroundColor: '#97294E'}}>
         <Navbar.Brand href="/">COVID-19 Infodemic</Navbar.Brand>
         <Nav className="mr-auto">
-            <Nav.Item><Link to='/'>Home</Link></Nav.Item>
-            <Nav.Item><Link to='/findstate'>Find State</Link></Nav.Item>
-            <Nav.Item><Link to='/aboutus'>About Us</Link></Nav.Item>
+            <Nav.Item><Nav.Link><Link style={{color: '#FFF'}} to='/'>Home</Link></Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link><Link style={{color: '#FFF'}} to='/findstate'>Find State</Link></Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link><Link style={{color: '#FFF'}} to='/aboutus'>About Us</Link></Nav.Link></Nav.Item>
             {/* <Nav.Item><Nav.Link href='/'>Home</Nav.Link></Nav.Item>
             <Nav.Item><Nav.Link href='/findstate'>Find State</Nav.Link></Nav.Item>
             <Nav.Item><Nav.Link href='/aboutus'>About Us</Nav.Link></Nav.Item> */}
