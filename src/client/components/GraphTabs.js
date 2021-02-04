fb2a59a2aa4a5c80ecd9d890d62543af2114ebdb
yhatori <yhatori@scu.edu>
2021-02-03 20:14:04 -0800

    minor adjusts to appearance

--- src/client/components/GraphTabs.js
+++ src/client/components/GraphTabs.js
@@ -10,7 +10,10 @@ const graphStyle = {
 
 const GraphTabs = (props) => {
     const { state } = props; 
-    const Arizona = [ require('../../images/AZ/AZ_cov_news1.png'), require('../../images/AZ/AZ_cov_news2.png'), require('../../images/AZ/AZ_cov_stop_4.png'), require('../../images/AZ/AZ_inst_trust1_2.png'), require('../../images/AZ/AZ_inst_trust1_5.png')];
+    const Arizona = [ require('../../images/AZ/AZ_cov_news1.png'), 
+    require('../../images/AZ/AZ_cov_news2.png'), require('../../images/AZ/AZ_cov_stop_4.png'), 
+    require('../../images/AZ/AZ_inst_trust1_2.png'), require('../../images/AZ/AZ_inst_trust1_5.png')];
+
     // const  imageMap= { Arizona: [require('../../images/AZ/AZ_cov_news1.png'), require('../../images/AZ/AZ_cov_news2.png'), require('../../images/AZ/AZ_cov_stop_4.png'), require('../../images/AZ/AZ_inst_trust1_2.png'), require('../../images/AZ/AZ_inst_trust1_5.png')]};
     const stateName = state.name; 
   return (
@@ -19,7 +22,9 @@ const GraphTabs = (props) => {
           <Tabs defaultActiveKey="survey">
               <Tab eventKey="all" title="All"><p>hello</p></Tab>
               <Tab eventKey="survey" title="Survey">
-                  <center><h4>In the last 24 hours, did you get any news or information related to the current coronavirus (COVID-19) outbreak from the following sources?</h4><img style={graphStyle} src={logo}/></center>
+                  <center><h4>In the last 24 hours, did you get any news or information related to the 
+                      current coronavirus (COVID-19) outbreak from the following sources?</h4>
+                      <img style={graphStyle} src={logo}/></center>
               </Tab>
               <Tab eventKey="timeline" title="COVID-19 Timeline"><p>hello</p></Tab>
             </Tabs>
