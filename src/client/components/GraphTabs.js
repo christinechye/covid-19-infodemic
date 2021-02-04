be557332aed8a5a00b77793e5d8177caeb8b6216
Christine Chye <ce.chye@gmail.com>
2021-02-03 17:19:33 -0800

    added graphs and working on debugging so that it will display for each state

--- src/client/components/GraphTabs.js
+++ src/client/components/GraphTabs.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
+// import Image from 'react-bootstrap/Image';
+import logo from '../../images/AZ/AZ_cov_news1.png';
+
+const graphStyle = { 
+    height: '400px', 
+}
 
 const GraphTabs = (props) => {
     const { state } = props; 
+    const Arizona = [ require('../../images/AZ/AZ_cov_news1.png'), require('../../images/AZ/AZ_cov_news2.png'), require('../../images/AZ/AZ_cov_stop_4.png'), require('../../images/AZ/AZ_inst_trust1_2.png'), require('../../images/AZ/AZ_inst_trust1_5.png')];
+    // const  imageMap= { Arizona: [require('../../images/AZ/AZ_cov_news1.png'), require('../../images/AZ/AZ_cov_news2.png'), require('../../images/AZ/AZ_cov_stop_4.png'), require('../../images/AZ/AZ_inst_trust1_2.png'), require('../../images/AZ/AZ_inst_trust1_5.png')]};
+    const stateName = state.name; 
   return (
       <React.Fragment>
           <h1>{state.name}</h1>
           <Tabs defaultActiveKey="survey">
               <Tab eventKey="all" title="All"><p>hello</p></Tab>
               <Tab eventKey="survey" title="Survey">
-                  <center><h4>In the last 24 hours, did you get any news or information related to the current coronavirus (COVID-19) outbreak from the following sources?</h4></center>
+                  <center><h4>In the last 24 hours, did you get any news or information related to the current coronavirus (COVID-19) outbreak from the following sources?</h4><img style={graphStyle} src={logo}/></center>
               </Tab>
               <Tab eventKey="timeline" title="COVID-19 Timeline"><p>hello</p></Tab>
             </Tabs>
